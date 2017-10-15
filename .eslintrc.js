28a5ef4eb1974744d20a78be224d0e559e2cd587
Pengcheng <pengchengcao@hotmail.com>
2017-10-15 14:12:27 +1100

    setupWebpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,6 @@ module.exports = {
   "plugins": [ "react" ],
   "rules": {
     "react/prop-types": ["off"],
-    "indent": ["error", 2],
     "linebreak-style": ["error","unix"],
     "quotes": ["error","single"],
     "semi": ["error","always"],
--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,52 @@ const path = require('path');
 const webpack = require('webpack');
 
 const config = {
-  resolve: {
-    modules: [path.resolve('./lib'), path.resolve('./node_modules')]
-  },
-  entry: {
-    vendor: [
-      'babel-polyfill',
-      'react',
-      'react-dom',
-      'prop-types',
-      'axios',
-      'lodash.debounce',
-      'lodash.pickby'
-    ],
-    app: ['./lib/renderers/dom.js']
-  },
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: '[name].js'
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['react', 'env', 'stage-2']
-        }
-      }
-    }]
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    })
-  ]
+	resolve: {
+		modules: [path.resolve('./lib'), path.resolve('./node_modules')]
+	},
+	devtool:'inline-source-map',
+	entry: {
+		vendor: [
+			'babel-polyfill',
+			'react',
+			'react-dom',
+			'prop-types',
+			'axios',
+			'lodash.debounce',
+			'lodash.pickby'
+		],
+		app: ['./lib/renderers/dom.js']
+	},
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: '[name].js'
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['react', 'env', 'stage-2']
+				}
+			}
+		}]
+	},
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor'
+		})
+	],
+	devServer: {
+		port: 3001,
+		proxy:{
+			'/':'http://localhost:8080'
+		},
+		open: true,
+		contentBase:[path.resolve(__dirname, 'lib/scss')],
+		watchContentBase: true
+	}
 };
 
 module.exports = config;
